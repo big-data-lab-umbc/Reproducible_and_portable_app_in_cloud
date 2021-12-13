b562ea747cbc578a4a3414e715102db986bfb15e
Starly <43221132+starlyxxx@users.noreply.github.com>
2021-12-13 14:32:56 -0500

    Update main.py
    default one_click option in main.py is False

--- main.py
+++ main.py
@@ -6,51 +6,49 @@ import os
 import uuid
 from subprocess import check_output, Popen, call, PIPE, STDOUT
 
-resconfigFile = "./ConfigTemplate/resource.ini"
-appconfigFile = "./ConfigTemplate/application.ini"
-personalconfigFile = "./ConfigTemplate/personal.ini"
-
+configFile = "./config/config.ini"
 reproduceFolder = "./reproduce"
-reproducePara = "pipeline_para.json"
-reproduceConf = "pipeline.json"
+reproducePara = "reproduce_para.json"
+reproduceConf = "reproduce_conf.json"
 reproduceEvent = "my_event.json"
 
 id_uuid = str(uuid.uuid4())
+one_click = False
 
 
 def readsummary(): 
     config = configparser.ConfigParser()
-    config.read(personalconfigFile)
-    return (str(config['summary']['cloud']),str(config['summary']['application']),str(config['summary']['your_key_path']),str(config['summary']['your_key_name']),str(config['summary']['your_git_username']),str(config['summary']['your_git_password']),str(config['summary']['cloud_credentials']))
+    config.read(configFile)
+    return (str(config['summary']['cloud']),str(config['summary']['application']),str(config['summary']['your_key_path']),str(config['summary']['your_key_name']),str(config['summary']['your_git_username']),str(config['summary']['your_git_password']),str(config['summary']['cloud_credentials']),str(config['summary']['git_link']),str(config['summary']['bootstrap']))
 
 def readparameter():
     config = configparser.ConfigParser() 
-    config.read(appconfigFile)
-    return (str(config['parameter']['experiment_docker']),str(config['parameter']['experiment_name']),str(config['parameter']['data']),str(config['parameter']['command']),str(config['parameter']['git_link']),str(config['parameter']['bootstrap']))
+    config.read(configFile)
+    return (str(config['parameter']['experiment_docker']),str(config['parameter']['experiment_name']),str(config['parameter']['data']),str(config['parameter']['command']))
 
 def readawscloud():
     config = configparser.ConfigParser() 
-    config.read(resconfigFile)
+    config.read(configFile)
     return (int(config['cloud.aws']['instance_num']),str(config['cloud.aws']['SUBNET_ID']),str(config['cloud.aws']['INSTANCE_TYPE']),str(config['cloud.aws']['VPC_ID']))
 
 def readazurecloud():
     config = configparser.ConfigParser() 
-    config.read(resconfigFile)
+    config.read(configFile)
     return (str(config['cloud.azure']['REGION']),str(config['cloud.azure']['resourceGroupID']),str(config['cloud.azure']['resourceGroupName']),str(config['cloud.azure']['instance_num']),str(config['cloud.azure']['INSTANCE_TYPE']))
 
-# def readbill():
-#     config = configparser.ConfigParser() 
-#     config.read(resconfigFile)
-#     return (float(config['bill']['vm_price']),float(config['bill']['bigdata_cluster_price']),float(config['bill']['network_price']),float(config['bill']['storage_price']),float(config['bill']['container_price']))
+def readbill():
+    config = configparser.ConfigParser() 
+    config.read(configFile)
+    return (float(config['bill']['vm_price']),float(config['bill']['bigdata_cluster_price']),float(config['bill']['network_price']),float(config['bill']['storage_price']),float(config['bill']['container_price']))
 
 def readreprodeuce():
     config = configparser.ConfigParser() 
-    config.read(resconfigFile)
+    config.read(configFile)
     return (str(config['reproduce']['reproduce_storage']),str(config['reproduce']['reproduce_database']))
 
-cloud_provider, application, your_key_path, your_key_name, your_git_username, your_git_password, cloud_credentials = readsummary()  #str
-# vm_price, bigdata_cluster_price, network_price, storage_price, container_price = readbill()  #float
-experiment_docker, experiment_name, data, command, git_link, bootstrap = readparameter()   #str
+cloud_provider, application, your_key_path, your_key_name, your_git_username, your_git_password, cloud_credentials, git_link, bootstrap = readsummary()  #str
+vm_price, bigdata_cluster_price, network_price, storage_price, container_price = readbill()  #float
+experiment_docker, experiment_name, data, command = readparameter()   #str
 reproduce_storage, reproduce_database = readreprodeuce()
 if cloud_provider == "aws":
     instance_num, SUBNET_ID, INSTANCE_TYPE, VPC_ID = readawscloud() #int,str
@@ -63,10 +61,11 @@ elif cloud_provider == "azure":
 class Aws:
     def __init__(self, name):
         self.name = name
-        self.app_path = "AwsServerlessTemplate/"+application+"/"     #SparkAnalytics/DaskAnalytics/HovorodAnalytics
+        self.app_path = "AwsServerlessTemplate/"+application+"/"     #bigdata_analytics/cpu_analytics/gpu_analytics
 
         #self.para_path = self.app_path+"parameter.json"
-        self.deploy_conf_path = self.app_path+"deploy_config.json"
+        self.depoly_conf_path = self.app_path+"depoly_config.json"
+        self.depoly_event_path = self.app_path+"SampleEvent.json"
 
     def __str__(self):
         return '{} the aws template'.format(self.name)
@@ -91,20 +90,22 @@ class Aws:
                 pass
         return event_dict
 
-    def aws_deploy(self):
+
+    def aws_depoly(self):
         #config
-        with open(self.deploy_conf_path, "r") as json_file:
-            deploy_conf_dict = json.load(json_file)
-        deploy_new_conf_dict = deploy_conf_dict
-        deploy_new_conf_dict = self.para_control(deploy_new_conf_dict,'InstanceType',INSTANCE_TYPE)
-        deploy_new_conf_dict = self.para_control(deploy_new_conf_dict,'InstanceNum',instance_num)
-        deploy_new_conf_dict = self.para_control(deploy_new_conf_dict,'Ec2KeyName',your_key_name)
-        deploy_new_conf_dict = self.para_control(deploy_new_conf_dict,'Ec2KeyPath',your_key_path)
-        deploy_new_conf_dict = self.para_control(deploy_new_conf_dict,'SubnetId',SUBNET_ID)
-        deploy_new_conf_dict = self.para_control(deploy_new_conf_dict,'VpcId',VPC_ID)
+        with open(self.depoly_conf_path, "r") as json_file:
+            depoly_conf_dict = json.load(json_file)
+        depoly_new_conf_dict = depoly_conf_dict
+        depoly_new_conf_dict = self.para_control(depoly_new_conf_dict,'InstanceType',INSTANCE_TYPE)
+        depoly_new_conf_dict = self.para_control(depoly_new_conf_dict,'InstanceNum',instance_num)
+        depoly_new_conf_dict = self.para_control(depoly_new_conf_dict,'Ec2KeyName',your_key_name)
+        depoly_new_conf_dict = self.para_control(depoly_new_conf_dict,'Ec2KeyPath',your_key_path)
+        depoly_new_conf_dict = self.para_control(depoly_new_conf_dict,'SubnetId',SUBNET_ID)
+        depoly_new_conf_dict = self.para_control(depoly_new_conf_dict,'VpcId',VPC_ID)
+        depoly_new_conf_dict = self.para_control(depoly_new_conf_dict,'SimpleTableName',reproduce_database)
         #TODO: generate security group resources for each test
         with open(reproduceFolder+"/"+reproduceConf, "w") as json_file:
-            json.dump(deploy_conf_dict, json_file, indent=4)
+            json.dump(depoly_new_conf_dict, json_file, indent=4)
 
         with open(self.depoly_event_path, "r") as json_file:
             event_dict = json.load(json_file)
@@ -122,17 +123,17 @@ class Aws:
         lambda_path = self.app_path+"lambda"
         target_path = reproduceFolder+"/lambda"
         shutil.copytree(lambda_path, target_path)
-        return 'start deploying on aws'
+        return 'start depolying on aws'
 
 class Azure:
     def __init__(self, name):
         self.name = name
-        self.app_path = "AzureServerlessTemplate/"+application+"/"     #SparkAnalytics/DaskAnalytics/HovorodAnalytics
+        self.app_path = "AzureServerlessTemplate/"+application+"/"     #bigdata_analytics/cpu_analytics/gpu_analytics
 
         self.para_path = self.app_path+"parameter.json"
-        self.deploy_conf_path = self.app_path+"deploy_config.json"
+        self.depoly_conf_path = self.app_path+"depoly_config.json"
         self.command_path = self.app_path+"command_script.sh"
-        self.lambda_path = self.app_path+"simu_lambda.sh"
+        self.lambda_path = self.app_path+"fake_lambda.sh"
 
     def __str__(self):
         return '{} the azure template'.format(self.name)
@@ -147,7 +148,7 @@ class Azure:
                 pass
         return para_dict
 
-    def azure_deploy(self):
+    def azure_depoly(self):
         #para
         with open(self.para_path, "r") as json_file:
             parameter_dict = json.load(json_file)
@@ -159,10 +160,10 @@ class Azure:
             json.dump(parameter_new_dict, json_file, indent=4)
 
         #config
-        with open(self.deploy_conf_path, "r") as json_file:
-            deploy_conf_dict = json.load(json_file)
+        with open(self.depoly_conf_path, "r") as json_file:
+            depoly_conf_dict = json.load(json_file)
         with open(reproduceFolder+"/"+reproduceConf, "w") as json_file:
-            json.dump(deploy_conf_dict, json_file, indent=4)
+            json.dump(depoly_conf_dict, json_file, indent=4)
 
         #command_script
         with open(self.command_path, "r") as bash_file:
@@ -170,7 +171,7 @@ class Azure:
         with open(reproduceFolder+"/reproduce_command.sh", "w") as bash_file:
             bash_file.writelines(command_script)
 
-        #simu_lambda
+        #fake_lambda
         with open(self.lambda_path, "r") as bash_file:
             lambda_script = bash_file.readlines()
         with open(reproduceFolder+"/reproduce_lambda.sh", "w") as bash_file:
@@ -181,7 +182,7 @@ class Azure:
         target_path = reproduceFolder+"/lambda"
         shutil.copytree(lambda_path, target_path)
 
-        return 'start deploying on azure'
+        return 'start depolying on azure'
 
 # class Provider:
 #     def __init__(self, name):
@@ -191,7 +192,7 @@ class Azure:
 #         return 'the {} provider'.format(self.name)
 
 #     def provider_deploy(self):
-#         return 'is deploying on provider'
+#         return 'is depolying on provider'
 
 class Adapter:
     def __init__(self, obj, adapted_methods):
@@ -203,6 +204,17 @@ class Adapter:
 
 def main():
 
+    # objects = [Computer('Asus')]
+
+    # synth = Synthesizer('moog')
+    # objects.append(Adapter(synth, dict(execute=synth.play)))
+
+    # human = Human('Bob')
+    # objects.append(Adapter(human, dict(execute=human.speak)))
+
+    # for i in objects:
+    #     print('{} {}'.format(str(i), i.execute()))
+
     for files in os.listdir(reproduceFolder):
         path = os.path.join(reproduceFolder, files)
         try:
@@ -212,35 +224,31 @@ def main():
 
     if cloud_provider == "aws":
         Cloud = Aws(application)
-        template=Adapter(Cloud, dict(execute=Cloud.aws_deploy))
+        template=Adapter(Cloud, dict(execute=Cloud.aws_depoly))
     elif cloud_provider == "azure":
         Cloud = Azure(application)
-        template=Adapter(Cloud, dict(execute=Cloud.azure_deploy))
+        template=Adapter(Cloud, dict(execute=Cloud.azure_depoly))
     else:
         raise SystemExit("NOT IMPLEMENTED CLOUD, currect support aws, azure")
 
     print('{} {}'.format(str(template), template.execute()))
 
     if cloud_provider == "aws":
-        call('aws configure set aws_access_key_id '+cloud_access_key, shell=True)
-        call('aws configure set aws_secret_access_key '+cloud_secret_key, shell=True)
-
         call('cd '+reproduceFolder+' && sam validate -t '+reproduceConf, shell=True)
         call('cd '+reproduceFolder+' && sam build -t '+reproduceConf, shell=True)
         call('cd '+reproduceFolder+' && sam deploy --stack-name samautoanalytics --s3-bucket %s --s3-prefix %s --capabilities CAPABILITY_IAM --no-confirm-changeset --debug --force-upload'%(reproduce_storage,id_uuid), shell=True)
         print("Resource provisioning success. Logs folder s3://%s/%s."%(reproduce_storage,id_uuid))
-        
-        with open(reproduceFolder+"/"+reproduceEvent, "r") as json_file:
-            event_dict = json.load(json_file)
-        event_detail = json.dumps(event_dict).replace('\"','\\"')
-        call('aws events put-events --entries \'[{"Source": "custom.reproduce", "DetailType": "RPAC", "Detail": "%s"}]\''%event_detail, shell=True)
-    
-    elif cloud_provider == "azure":
-        call('az login', shell=True)
 
+        if one_click:
+            with open(reproduceFolder+"/"+reproduceEvent, "r") as json_file:
+                event_dict = json.load(json_file)
+            event_detail = json.dumps(event_dict).replace('\"','\\"')
+            call('aws events put-events --entries \'[{"Source": "custom.reproduce", "DetailType": "RPAC", "Detail": "%s"}]\''%event_detail, shell=True)
+            print("Cloud execution start.")
+            
+    elif cloud_provider == "azure":
         call('cd '+reproduceFolder+' && az deployment group create --name Deploy LocalTemplate --resource-group %s --template-file %s --parameters %s --debug'%(resourceGroupName,reproduceConf,reproducePara), shell=True)
 
-    print("Cloud execution start.")
 
 if __name__ == "__main__":
     main()
